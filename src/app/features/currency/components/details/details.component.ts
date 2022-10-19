6f2186a59cf248623cca810566da2fa201e66a9f
Amit Kumar <amit.kumar33@nagarro.com>
2022-10-19 18:01:24 +0530

    restructured scss styling code, replaced ids with classes and corrected namings for classes

--- src/app/features/currency/components/details/details.component.ts
+++ src/app/features/currency/components/details/details.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { historicalDates, months } from 'src/app/contants';
-import { CurrencyConversionresponse } from 'src/app/models/response.model';
-import { CurrencyService } from 'src/app/services/currency.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { concatMap, forkJoin, ReplaySubject, takeUntil } from 'rxjs';
+import { historicalDates, months } from '../../constants';
+import { CurrencyConversionresponse } from '../../models/response.model';
+import { CurrencyService } from '../../services/currency.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   amount = new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+(\.?[0-9]+)?$/)]);
   from = new FormControl('');
   to = new FormControl('');
@@ -20,75 +21,81 @@ export class DetailsComponent implements OnInit {
   currencySymbols = ['']
   historicaldata: any[] = [];
   historicalMonths = months.map((m, i) => months[parseInt(historicalDates[i].split('-')[1]) - 1]);
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   constructor(private route: ActivatedRoute, private router: Router, private currencySevice: CurrencyService,) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      console.log(params);
-      this.from.setValue(params.get('from'));
-      this.to.setValue(params.get('to'));
-      this.amount.setValue(params.get('amount'));
-      this.convertedCurrency.setValue(params.get('convertedCurrency'));
-      this.rate.setValue(params.get('rate'));
-      this.getCurrencySymbols();
-      this.gethistoricalData();
-    });
+    this.route.paramMap
+      .pipe(
+        concatMap(params => {
+          this.setValues(params);
+          return forkJoin([
+            this.currencySevice.getCurrencySymbols(),
+            this.currencySevice.requestDataForHistoricalDates(this.from.value, this.to.value, historicalDates)
+          ]);
+        })
+      )
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((response: any[]) => {
+        if (response && response.length == 2 && response[0].symbols && typeof response[0].symbols == 'object' && response[1].length > 0) {
+          this.currencySymbols = Object.keys(response[0].symbols);
+          this.title = `${this.from.value} - ${response[0].symbols[this.from.value]}`;
+          this.historicaldata = response[1].map((x: any) => x.rates[this.to.value]);
+        }
+      })
+  }
+  /**
+  * Unsubscribe all the subscriptions when component gets destroyed
+  */
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.unsubscribe();
   }
 
   /**
-* For getting list of all the currencies
-*/
-  getCurrencySymbols() {
-    this.currencySevice.getCurrencySymbols().subscribe((response: any) => {
-      if(response && response.symbols && typeof response.symbols == 'object') {
-        this.currencySymbols = Object.keys(response.symbols)
-        this.amount.setValue(this.amount.value);
-        this.from.setValue(this.from.value);
-        this.to.setValue(this.to.value);
-        this.title = `${this.from.value} - ${response.symbols[this.from.value]}`
-      }
-
-    })
-
-
+  * For setting form values
+  */
+  setValues(params: ParamMap) {
+    this.from.setValue(params.get('from'));
+    this.to.setValue(params.get('to'));
+    this.amount.setValue(params.get('amount'));
+    this.convertedCurrency.setValue(params.get('convertedCurrency'));
+    this.rate.setValue(params.get('rate'));
   }
 
   /**
-* For navigating to the Details page
-*/
+  * For navigating to the Details page
+  */
   redirectToHome() {
     this.router.navigate(['/home']);
   }
 
   /**
-* For clearing/resetting the amount
-*/
+  * For clearing/resetting the amount
+  */
   resetAmount() {
     this.amount.setValue('');
   }
 
   /**
-* For converion from base currency to a desired currency
-*/
+  * For converion from base currency to a desired currency
+  */
   convert() {
-    this.currencySevice.getConversion(this.amount.value, this.from.value, this.to.value).subscribe((response: CurrencyConversionresponse) => {
-      if (response && response.info && response.info.rate && response.result) {
-        this.rate.setValue(`1.00 ${this.from.value} = ${response.info.rate} ${this.to.value}`);
-        this.convertedCurrency.setValue(`${response.result} ${this.to.value}`);
-        this.gethistoricalData();
-      }
-
-    })
-  }
-
-  /**
-* For getting the historical data of the base currency
-*/
-  gethistoricalData() {
-    this.currencySevice.requestDataForHistoricalDates(this.from.value, this.to.value, historicalDates).subscribe((response: any[]) => {
-      if (response && response.length > 0) {
-        this.historicaldata = response.map((x: any) => x.rates[this.to.value]);
-      }
-    })
+    this.currencySevice.getConversion(this.amount.value, this.from.value, this.to.value)
+      .pipe(
+        concatMap((response: CurrencyConversionresponse) => {
+          if (response && response.info && response.info.rate && response.result) {
+            this.rate.setValue(`1.00 ${this.from.value} = ${response.info.rate} ${this.to.value}`);
+            this.convertedCurrency.setValue(`${response.result} ${this.to.value}`);
+          }
+          return this.currencySevice.requestDataForHistoricalDates(this.from.value, this.to.value, historicalDates)
+        })
+      )
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(response => {
+        if (response && response.length > 0) {
+          this.historicaldata = response.map((x: any) => x.rates[this.to.value]);
+        }
+      })
   }
 }
--- src/app/features/currency/components/home/home.component.ts
+++ src/app/features/currency/components/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CurrencyConversionresponse } from 'src/app/models/response.model';
-import { CurrencyService } from 'src/app/services/currency.service';
-
+import { Router } from '@angular/router';
+import { concatMap, ReplaySubject, takeUntil } from 'rxjs';
+import { popularCurrencies } from '../../constants';
+import { CurrencyConversionresponse } from '../../models/response.model';
+import { CurrencyService } from '../../services/currency.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   amount = new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+(\.?[0-9]+)?$/)]);
   from = new FormControl('');
   to = new FormControl('');
@@ -18,7 +19,8 @@ export class HomeComponent implements OnInit {
 
   currencySymbols: any[] = [];
   popularConversions: any[] = [];
-  popularCurrencies = ['INR', 'USD', 'EUR', 'AED', 'SGD', 'CNY', 'AUD', 'RUB', 'GBP']
+  popularCurrencies = popularCurrencies;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private currencySevice: CurrencyService,
     private router: Router) { }
@@ -26,73 +28,75 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.getCurrencySymbols();
   }
+  /**
+ * Unsubscribe all the subscriptions when component gets destroyed
+ */
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.unsubscribe();
+  }
 
   /**
-* For getting list of all the currencies
-*/
+  * For getting list of all the currencies
+  */
   getCurrencySymbols() {
-    this.currencySevice.getCurrencySymbols().subscribe((response: any) => {
-      if (typeof response.symbols == 'object')
-        this.currencySymbols = Object.keys(response.symbols)
-      console.log(response.symbols)
-      this.amount.setValue(1);
-      this.from.setValue('EUR');
-      this.to.setValue('USD');
+    this.currencySevice.getCurrencySymbols()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((response: any) => {
+        if (typeof response.symbols == 'object')
+          this.currencySymbols = Object.keys(response.symbols)
+        this.amount.setValue(1);
+        this.from.setValue('EUR');
+        this.to.setValue('USD');
 
-    })
+      })
 
   }
 
   /**
-* For converion from base currency to a desired currency
-*/
+  * For converion from base currency to a desired currency
+  */
   convert() {
-    this.currencySevice.getConversion(this.amount.value, this.from.value, this.to.value).subscribe((response: CurrencyConversionresponse) => {
-      if (response && response.info && response.info.rate && response.result) {
-        this.rate.setValue(`1.00 ${this.from.value} = ${response.info.rate} ${this.to.value}`);
-        this.convertedCurrency.setValue(`${response.result} ${this.to.value}`)
-        this.ConvertToPopularCurrecies();
-      }
-
-    })
-  }
-
-  /**
-* For converion from base currency to a list of currencies
-*/
-  ConvertToPopularCurrecies() {
-    const popularCurrencies = this.popularCurrencies.join(',');
-    this.currencySevice.getPopularCurrencyConversions(this.from.value, popularCurrencies).subscribe((response: any) => {
-      console.log(response.rates);
-
-      this.popularConversions.length = 0;
-      if (response && response.rates && typeof response.rates == 'object') {
-        Object.keys(response.rates).forEach((key) => {
-          this.popularConversions.push({ symbol: key, rate: (response?.rates?.[key] * this.amount.value).toFixed(2) })
+    this.currencySevice.getConversion(this.amount.value, this.from.value, this.to.value)
+      .pipe(
+        concatMap((response: CurrencyConversionresponse) => {
+          if (response && response.info && response.info.rate && response.result) {
+            this.rate.setValue(`1.00 ${this.from.value} = ${response.info.rate} ${this.to.value}`);
+            this.convertedCurrency.setValue(`${response.result} ${this.to.value}`)
+          }
+          const popularCurrencies = this.popularCurrencies.join(',');
+          return this.currencySevice.getPopularCurrencyConversions(this.from.value, popularCurrencies)
         })
-      }
-
-    })
+      )
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((response: any) => {
+        this.popularConversions.length = 0;
+        if (response && response.rates && typeof response.rates == 'object') {
+          Object.keys(response.rates).forEach((key) => {
+            this.popularConversions.push({ symbol: key, rate: (response?.rates?.[key] * this.amount.value).toFixed(2) })
+          })
+        }
+      })
   }
 
   /**
-* For clearing/resetting the amount
-*/
+  * For clearing/resetting the amount
+  */
   resetAmount() {
     this.amount.setValue('');
   }
 
   /**
-* For navigating to the Details page
-*/
+  * For navigating to the Details page
+  */
   redirectToDetails() {
     const param = { from: this.from.value, to: this.to.value, amount: this.amount.value, rate: this.rate.value, convertedCurrency: this.convertedCurrency.value };
     this.router.navigate(['/details', param])
   }
 
   /**
-* For swapping the to and from currency
-*/
+  * For swapping the to and from currency
+  */
   swapCurrencySymbols() {
     const temp = this.from.value;
     this.from.setValue(this.to.value);
